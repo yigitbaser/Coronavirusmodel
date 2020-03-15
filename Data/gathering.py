128fff1dd7b938d127b20d955790b3a5a4980243
Yigit Baser <yigit.baser92@gmail.com>
2020-03-15 02:41:59 +0100

    utils

--- Data/gathering.py
+++ Data/gathering.py
@@ -1,3 +1,17 @@
 import pandas as pd
 
-ncov_data = 
+
+ncov_data_cases = pd.read_html('https://www.worldometers.info/coronavirus/', )
+
+print(ncov_data_cases)
+
+df = pd.DataFrame(ncov_data_cases)
+print(df)
+
+df.to_csv('/Users/yigitbaser/Coronavirusmodel/dataCSV.csv', index=True)
+
+
+
+#t = ncov_data.pd.DataFrame.to_csv("data")
+#ncov_data.to_csv('/Users/yigitbaser/Coronavirusmodel', index=True)
+
