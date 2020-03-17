7451f49bf8fab227d442138a79fd5660724d35f4
Yigit Baser <yigit.baser92@gmail.com>
2020-03-17 15:49:06 +0100

    Add get data functions

--- Data/gathering.py
+++ Data/gathering.py
@@ -2,45 +2,51 @@
 Gathers data from www.worldometers.info
 
 """
-
+#TODO Write docstrings
 import pandas as pd
 
-URL_ADD = 'https://www.worldometers.info/coronavirus'
-ncov_data_cases = pd.read_html(URL_ADD)
+URL_GENERAL_DATA = 'https://www.worldometers.info/coronavirus'
+URL_AGE_SEX_DATA = 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/'
+URL_DEATH_TOLL = 'https://www.worldometers.info/coronavirus/coronavirus-death-toll/'
 
-ncov_data_df = ncov_data_cases[0]
+def gather_total_data() -> pd.DataFrame:
+    """
 
-
-def gather_total_data():
-    URL_ADD = 'https://www.worldometers.info/coronavirus'
-    ncov_data_cases = pd.read_html(URL_ADD)
+    :return: DataFrame.
+    """
+    ncov_data_cases = pd.read_html(URL_GENERAL_DATA)
     ncov_data_df = ncov_data_cases[0]
     return ncov_data_df
 
-def gather_age_data():
-    URL_ADD = 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/'
-    ncov_data_age = pd.read_html(URL_ADD)
+def gather_age_data() -> pd.DataFrame:
+    """
+
+    :return:
+    """
+    ncov_data_age = pd.read_html(URL_AGE_SEX_DATA)
     ncov_data_age_df = ncov_data_age[0]
     return ncov_data_age_df
 
-def gather_sex_data():
-    URL_ADD = 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/'
-    ncov_data_sex = pd.read_html(URL_ADD)
+def gather_sex_data() -> pd.DataFrame:
+    ncov_data_sex = pd.read_html(URL_AGE_SEX_DATA)
     ncov_data_sex_df = ncov_data_sex[1]
     return ncov_data_sex_df
 
-def gather_precondition_data():
-    URL_ADD = 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/'
-    ncov_data_precon = pd.read_html(URL_ADD)
+def gather_precondition_data() -> pd.DataFrame:
+    ncov_data_precon = pd.read_html(URL_AGE_SEX_DATA)
     ncov_data_precon_df = ncov_data_precon[2]
     return ncov_data_precon_df
 
+def get_deathtoll_data():
+    dt_data = pd.read_html('https://www.worldometers.info/coronavirus/coronavirus-death-toll/')
+    return dt_data[0]
 
 
 
 
 
-print(ncov_data_df[ncov_data_df['Country,Other']== 'Iran']['TotalCases'])
+
+#print(NCOV_DATA_DF[NCOV_DATA_DF['Country,Other'] == 'Iran']['TotalCases'])
 
 
 
@@ -52,4 +58,3 @@ print(ncov_data_df[ncov_data_df['Country,Other']== 'Iran']['TotalCases'])
 
 #t = ncov_data.pd.DataFrame.to_csv("data")
 #ncov_data.to_csv('/Users/yigitbaser/Coronavirusmodel', index=True)
-
