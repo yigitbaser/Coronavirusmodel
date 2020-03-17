8a6495eeb6dec02fd6c2023bf13c6746719ddeec
Yigit Baser <yigit.baser92@gmail.com>
2020-03-17 12:41:53 +0100

    Gather tables

--- Data/gathering.py
+++ Data/gathering.py
@@ -9,8 +9,36 @@ URL_ADD = 'https://www.worldometers.info/coronavirus'
 ncov_data_cases = pd.read_html(URL_ADD)
 
 ncov_data_df = ncov_data_cases[0]
-print(type(ncov_data_df))
-#print(ncov_data_cases)
+
+
+def gather_total_data():
+    URL_ADD = 'https://www.worldometers.info/coronavirus'
+    ncov_data_cases = pd.read_html(URL_ADD)
+    ncov_data_df = ncov_data_cases[0]
+    return ncov_data_df
+
+def gather_age_data():
+    URL_ADD = 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/'
+    ncov_data_age = pd.read_html(URL_ADD)
+    ncov_data_age_df = ncov_data_age[0]
+    return ncov_data_age_df
+
+def gather_sex_data():
+    URL_ADD = 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/'
+    ncov_data_sex = pd.read_html(URL_ADD)
+    ncov_data_sex_df = ncov_data_sex[1]
+    return ncov_data_sex_df
+
+def gather_precondition_data():
+    URL_ADD = 'https://www.worldometers.info/coronavirus/coronavirus-age-sex-demographics/'
+    ncov_data_precon = pd.read_html(URL_ADD)
+    ncov_data_precon_df = ncov_data_precon[2]
+    return ncov_data_precon_df
+
+
+
+
+
 
 print(ncov_data_df[ncov_data_df['Country,Other']== 'Iran']['TotalCases'])
 
