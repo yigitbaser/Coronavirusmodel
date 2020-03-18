681d009bc026d4d815a4a0a22614912c4d3bd3d4
Yigit Baser <yigit.baser92@gmail.com>
2020-03-18 15:18:56 +0100

    saverloader initial

---
+++ src/Utils/SaverAndLoader.py
@@ -0,0 +1,12 @@
+from src.Data.gathering import *
+
+from pandas import DataFrame
+import datetime
+
+import pandas as pd
+if __name__ == '__main__':
+    data = get_total_data()
+
+    data.to_csv('')
+
+
