662b8f7c7adedc177eba4472d836815d50bc76a3
Yigit Baser <yigit.baser92@gmail.com>
2020-03-22 07:00:27 +0100

    some plot code

--- src/data/visualization.py
+++ src/data/visualization.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import src.Utils.Loader
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import gathering
 
-plt.close('all')
 
 plt.figure()
 df = pd.read_csv('/Users/yigitbaser/Coronavirusmodel/Storage/2020-03-18/TotalTable_1933.csv')
-#df.plot(x='Country,Other',y='TotalCases',kind='scatter')
-#plt.show()
+df.plot(x='Country,Other',y='TotalCases',kind='scatter')
+plt.show()
