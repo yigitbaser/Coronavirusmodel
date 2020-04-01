b6b80c1fa228419cf77461bcfc78aa868006155d
Yigit Baser <yigit.baser92@gmail.com>
2020-04-01 15:46:15 +0200

    update Tests

--- tests/t_Utils/test_SaverUtils.py
+++ tests/t_Utils/test_SaverUtils.py
@@ -8,18 +8,22 @@ TABLE_PATH = "/Users/yigitbaser/Coronavirusmodel/Storage/2020-03-22/TotalTable_7
 
 def test_add_Timestamp():
     """
-    Tests if added column 'Timestamp includes timestamp objects'
+    Tests if added column is added then tests column 'Timestamp' includes timestamp objects.
     :return:
     """
     table_to_add = Loader.load_data(file_path=TABLE_PATH)
     added = Saver.add_Timestamp(data_input=table_to_add)
+    assert 'Timestamp' in added.columns
 
+    #Check if column 'Timestamp' contains df.Timestamp objects
     assert added['Timestamp'].dtype.str[1] == 'M'
+
 
 def test_save_to_directory():
     """
 
     """
+    pass
 
 
 
